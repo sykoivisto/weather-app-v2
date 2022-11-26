7824fcf0d74c5cd7b6d25fdeb82b08f781c853ab
sy <sykoivisto@gmail.com>
2022-11-26 16:18:59 -0700

    try getting the weather async

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 //get the weather data
 const getWeatherData = async (city, units) => {
-    const response = await fetch(`https://weather-app-api-proxy.onrender.com/api?q=${city}&units=${units}`, { mode: 'cors' });
-    const data = await response.json();
-    return data;
+  const response = await fetch(
+    `https://weather-app-api-proxy.onrender.com/api?q=${city}&units=${units}`,
+    { mode: "cors" }
+  );
+  const data = await response.json();
+  return {
+    data,
+    units,
+  };
 };
 
 //give the weather data, and return just the info we want.
-const processWeatherData = (weatherData) => {
-    const location = weatherData.name;
-    const conditions = weatherData.weather[0].description;
-    const temp = weatherData.main.temp;
-    const feelsLike = weatherData.main.feels_like;
-    const humidity = weatherData.main.humidity;
-    const wind = weatherData.wind.speed;
+const processWeatherData = (weatherData, units) => {
+  const location = weatherData.name;
+  const conditions = weatherData.weather[0].description;
+  const temp = weatherData.main.temp;
+  const feelsLike = weatherData.main.feels_like;
+  const humidity = weatherData.main.humidity;
+  const wind = weatherData.wind.speed;
 
-    return {
-        location,
-        conditions,
-        temp,
-        feelsLike,
-        humidity,
-        wind
-    };
+  return {
+    location,
+    conditions,
+    temp,
+    feelsLike,
+    humidity,
+    wind,
+    units,
+  };
 };
 
 //calling the weather manually
-getWeatherData('boston', 'metric');
+const getWeather = async () => {
+  const weatherData = await getWeatherData("boston", "imperial");
+
+  const processedData = processWeatherData(weatherData.data, weatherData.units);
+
+  console.log(processedData);
+};
+
+getWeather();
