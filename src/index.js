8f4371edcdd4215760973f3e52742e0e2302944c
sy <sykoivisto@gmail.com>
2022-11-26 21:25:28 -0700

    install webpack/ dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-//get the weather data
+import "./index.css";
+
+// get the weather data
 const getWeatherData = async (city, units) => {
   const response = await fetch(
     `https://weather-app-api-proxy.onrender.com/api?q=${city}&units=${units}`,
@@ -11,13 +13,13 @@ const getWeatherData = async (city, units) => {
   };
 };
 
-//give the weather data, and return just the info we want.
+// give the weather data, and return just the info we want.
 const processWeatherData = (weatherData, units) => {
   const location = weatherData.name;
   const conditions = weatherData.weather[0].description;
-  const temp = weatherData.main.temp;
+  const { temp } = weatherData.main;
   const feelsLike = weatherData.main.feels_like;
-  const humidity = weatherData.main.humidity;
+  const { humidity } = weatherData.main;
   const wind = weatherData.wind.speed;
 
   return {
@@ -31,7 +33,7 @@ const processWeatherData = (weatherData, units) => {
   };
 };
 
-//calling the weather manually
+// calling the weather manually
 const getWeather = async () => {
   const weatherData = await getWeatherData("boston", "imperial");
 
